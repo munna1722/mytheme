63616fbe40f038d4bcf9f9f1d8f6ef788a69be5c
Munna <ibnesinamunna@gmail.com>
2024-11-14 16:06:18 +0600

    post format

--- template-parts/contact-page.php
+++ template-parts/contact-page.php
@@ -2,21 +2,15 @@
 
   <header class="entry-header">
 
-    <span class="dashicons dashicons-format-<?php echo get_post_format( $post->ID ); ?>"></span>
-    
-
     <?php the_title( '<h1>', '</h1>' ); ?>
-
-    <div class="byline">
-      <?php esc_html_e( 'Author:' ); ?> <?php the_author(); ?>      
-    </div>
 
   </header>
 
   <div class="entry-content">
-
     <?php the_content(); ?>
-
   </div>
-
+    
+    <?php if(comments_open()) : ?>
+      <?php comments_template(); ?>
+    <?php endif; ?>
 </article>
